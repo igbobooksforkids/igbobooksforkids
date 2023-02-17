a47ec7ab1db295b732000db666a274c9e1162888
igbobooksforkids <125662724+igbobooksforkids@users.noreply.github.com>
2023-02-17 14:21:52 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
-
-
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
